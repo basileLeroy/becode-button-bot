6089b7f45ecf20e059246543483336dc1871c8dc
basileLeroy <basile.leroy.jnm@hotmail.com>
2021-12-24 13:16:49 +0100

    Adding new text promotion

--- generateMessage.js
+++ generateMessage.js
@@ -1,5 +1,65 @@
-const messageGenerator = () => {
-    return console.log('test');
+const buildMessage = role => {
+    const pressDaButton = [
+        "Time to press your favourite button   👉🔴",
+        "Remember da button   👉🔴",
+        "It's button time!   👉🔴",
+        "Push it push it push it push it   👉🔴"
+    ];
+
+    const morningStart = [
+        `Gooooooood morning ${role} 😎`,
+        `Top of the morning ${role} ☀️`,
+        `Rise and shine ${role} 🤖`,
+        `Wakey, wakey, eggs and bakey ${role} 🍳`
+    ];
+
+    const morningEnd = [
+        `Congrats on making it through the morning ${role}!`,
+        `Half way done ${role}!`,
+        `${role}, make some space for snacks!`,
+        `Good job ${role}!`
+    ];
+
+    const afternoonStart = [
+        `Good afternoon ${role} 🕶️`,
+        `Pleasant afternoon we’re having, ${role} ☺️`,
+        `How u doin' ${role} 🤓`,
+        `Let's get started again ${role} 🚀`
+    ];
+
+    const afternoonEnd = [
+        `Well done today ${role}!`,
+        `You made it through ${role}!`,
+        `${role}, it's over, pat yourself!`,
+        `Amazing job you did ${role}!`
+    ];
+
+    const randomFrom = messages => {
+        const index = Math.floor(Math.random() * Math.floor(messages.length));
+        return messages[index];
+    };
+
+    const bruDateString = new Date().toLocaleString("en-US", {
+        timeZone: "Europe/Brussels"
+    });
+    const bruDate = new Date(bruDateString);
+    const bruHours = bruDate.getHours();
+    let message = "";
+
+    message = randomFrom(morningStart);
+
+    // if (bruHours < 12) {
+    //     message = randomFrom(morningStart);
+    // } else if (bruHours < 13) {
+    //     message = randomFrom(morningEnd);
+    // } else if (bruHours < 16) {
+    //     message = randomFrom(afternoonStart);
+    // } else {
+    //     message = randomFrom(afternoonEnd);
+    // }
+
+    // console.log(message + " " + randomFrom(pressDaButton));
+    return message + " " + randomFrom(pressDaButton);
 }
 
-export default messageGenerator;
+module.exports = {buildMessage};
--- server.js
+++ server.js
@@ -2,17 +2,34 @@
 
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
-import messageGenerator from './generateMessage';
+const { promotions } = require('./promotions');
+const { buildMessage } = require('./generateMessage');
+// import promotions from './promotions';
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS],
+    allowedMentions: { parse: ['users', 'roles'] }
+});
 
+// Get the messages
+const getMessages = () => {
+    // Get the promotions
+    promotions.forEach((promo, index) => {
+        const guild = promo.guild_id;
+        const discordChannel = promo.channel_id;
+        const role = promo.role_id
+
+        const unfilteredMessage = buildMessage(promo.name);
+        console.log(unfilteredMessage);
+    });
+}
+
+getMessages();
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
-    messageGenerator();
 });
-
 
 
 // Login to Discord with your client's token
